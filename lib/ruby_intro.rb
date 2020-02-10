3123184e6526b6b3d0ed74c88e958ebca52f4c60
testrDEV <devanshdas08@gmail.com>
2020-02-09 21:47:44 -0500

    Attempt 1 for Ruby HW1
    Home work submission for CS 545 Software engineering course at Binghamton University

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,79 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+sum_elements = 0
+  endResult = 0
+  if arr.length > 0 then
+    arr.each do |item|
+      sum_elements += item
+    end
+    endResult = sum_elements
+  end
+  return endResult
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  p=0
+  if arr.length > 0 then
+    p=arr.max(2).reduce(:+)
+    return p
+  end
+  return p
 end
+
+
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+ return false if arr.empty? && n.zero?
+  arr.combination(2).any? {|x, y| x + y == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+ 
+  "Hello, " + name 
+
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if /^[^aeiou\W]/i.match(s) != nil
+    return true
+  else
+    return false
+  end
 end
 
+
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  
+  return false if s.empty? 
+ 
+  if /^(0|(10|111*0)(10|111*0)*0)*$/.match(s) != nil  
+    return true
+  
+  else
+    return false
+  end 
+  
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  # initialize method 
+  def initialize(isbn, price) 
+   # variables 
+    @isbn = isbn 
+    @price = price 
+    if isbn.empty? || price<= 0
+      raise ArgumentError
+    end
+  end
+  
+  def price_as_string
+    "$%0.2f" % @price
+  end 
 end
